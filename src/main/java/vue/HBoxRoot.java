08e0879bff7d8d7b12bf1d5c5eccb9197cd522af
AthouNourane <nourane.athoumani@gmail.com>
2025-05-16 18:17:08 +0200

    Ajout Padding entre les modules

--- src/main/java/vue/HBoxRoot.java
+++ src/main/java/vue/HBoxRoot.java
@@ -5,9 +5,9 @@ import javafx.scene.layout.HBox;
 public class HBoxRoot extends HBox {
 
     public HBoxRoot(){
+        super(10);
         VBoxCalendrier calendrier = new VBoxCalendrier();
         GridPaneReservation reservation = new GridPaneReservation();
-
 
         this.getChildren().add(calendrier);
         this.getChildren().add(reservation);
