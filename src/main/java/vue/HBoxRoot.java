514ca75740539f7bb0c3d0378431490a687cdf6b
AthouNourane <nourane.athoumani@gmail.com>
2025-05-16 16:07:09 +0200

    Instanciation du calendrier et de la reservation

---
+++ src/main/java/vue/HBoxRoot.java
@@ -0,0 +1,14 @@
+package vue;
+
+import javafx.scene.layout.HBox;
+
+public class HBoxRoot extends HBox {
+
+    public HBoxRoot(){
+        VBoxCalendrier calendrier = new VBoxCalendrier();
+        GridPaneReservation reservation = new GridPaneReservation();
+
+        this.getChildren().add(calendrier);
+        this.getChildren().add(reservation);
+    }
+}
