50ac1d43b1d8bcbcfd6f8d64f2af12c29ad2dd70
AthouNourane <nourane.athoumani@gmail.com>
2025-05-16 19:06:04 +0200

    Ajout champs static et accesseurs

--- src/main/java/vue/HBoxRoot.java
+++ src/main/java/vue/HBoxRoot.java
@@ -1,15 +1,40 @@
 package vue;
 
+import controleur.Controleur;
 import javafx.scene.layout.HBox;
+import modele.PlanningCollections;
 
 public class HBoxRoot extends HBox {
+    private static PlanningCollections planning;
+    private static Controleur controleur;
+    private static VBoxCalendrier calendrierPane;
+    private static GridPaneReservation reservationPane;
 
     public HBoxRoot(){
         super(10);
-        VBoxCalendrier calendrier = new VBoxCalendrier();
-        GridPaneReservation reservation = new GridPaneReservation();
 
-        this.getChildren().add(calendrier);
-        this.getChildren().add(reservation);
+        planning = new PlanningCollections();
+        calendrierPane = new VBoxCalendrier();
+        controleur = new Controleur();
+        reservationPane = new GridPaneReservation();
+
+        this.getChildren().add(calendrierPane);
+        this.getChildren().add(reservationPane);
+    }
+
+    public static PlanningCollections getPlanning() {
+        return planning;
+    }
+
+    public static VBoxCalendrier getCalendrierPane(){
+        return calendrierPane;
+    }
+
+    public static Controleur getControleur(){
+        return controleur;
+    }
+
+    public static GridPaneReservation getReservationPane(){
+        return reservationPane;
     }
 }
