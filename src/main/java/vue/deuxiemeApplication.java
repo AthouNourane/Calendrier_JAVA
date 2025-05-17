82e6b3f3085654514d9e207f06d0e318d1fd7e44
AthouNourane <nourane.athoumani@gmail.com>
2025-05-18 00:43:31 +0200

    Taille fenêtre : 1000 → 1100

--- src/main/java/vue/deuxiemeApplication.java
+++ src/main/java/vue/deuxiemeApplication.java
@@ -11,7 +11,7 @@ import java.io.File;
 public class deuxiemeApplication extends Application {
     public void start(Stage stage)  {
         HBox root = new HBoxRoot();
-        Scene scene = new Scene(root, 1000, 300);
+        Scene scene = new Scene(root, 1100, 300);
 
         stage.setScene(scene);
         stage.setTitle ("Projet Réservation");
