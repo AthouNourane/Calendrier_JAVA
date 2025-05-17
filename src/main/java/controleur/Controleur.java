b9b34849d4693ad586aa7600b7416e1f001dfb69
AthouNourane <nourane.athoumani@gmail.com>
2025-05-17 20:47:43 +0200

    Controleur ajoute les réservations dans le planning

--- src/main/java/controleur/Controleur.java
+++ src/main/java/controleur/Controleur.java
@@ -4,8 +4,7 @@ import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.scene.control.Button;
 import javafx.scene.control.ToggleButton;
-import modele.DateCalendrier;
-import modele.PlanningCollections;
+import modele.*;
 import vue.*;
 
 public class Controleur implements EventHandler {
@@ -22,11 +21,25 @@ public class Controleur implements EventHandler {
             System.out.println(selDate.toString());
             reservationPane.updateDateSel(selDate);
         }
-
         // la source de event est le bouton "Enregistrer" du formulaire de réservation
         if (event.getSource() instanceof Button) {
-
-            // à compléter question 6
+            DateCalendrier dateReservation = reservationPane.getSelDate();
+            String coursReservation = reservationPane.getTextCours();
+            String niveauReservation = reservationPane.getNiveau();
+            Horaire heureDebut = new Horaire(reservationPane.getHeureDebut(), reservationPane.getMinDebut());
+            Horaire heureFin = new Horaire(reservationPane.getHeureFin(), reservationPane.getMinFin());
+            try {
+                PlageHoraire plageReservation = new PlageHoraire(heureDebut, heureFin);
+                Reservation reservation = new Reservation(dateReservation, plageReservation, coursReservation, niveauReservation);
+                planning.ajout(reservation);
+            } catch (ExceptionHoraire e) {
+                System.out.println("Erreur sur les horaires !");
+            } catch (ExceptionReservation e) {
+                System.out.println("Erreur sur la reservation !");
+            } catch (ExceptionPlanning e) {
+                System.out.println("Erreur lors de l'ajout dans le planning !");
+            }
+            System.out.println(planning);
         }
     }
 }
