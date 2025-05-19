ccaf07e6dbc23a4024da799c14fa17c0a9a0008c
AthouNourane <nourane.athoumani@gmail.com>
2025-05-19 23:39:06 +0200

    Ajout logo dans en-tête fenêtre

--- src/main/java/vue/deuxiemeApplication.java
+++ src/main/java/vue/deuxiemeApplication.java
@@ -2,19 +2,25 @@ package vue;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 import java.io.File;
+import java.util.Objects;
 
 public class deuxiemeApplication extends Application {
     public void start(Stage stage)  {
         HBox root = new HBoxRoot();
         Scene scene = new Scene(root, 1250, 500);
 
+        Image icon = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/images/agenda_logo.png")));
+        stage.getIcons().add(icon);
         stage.setScene(scene);
         stage.setTitle ("Projet Réservation");
         stage.show();
         stage.setResizable(false);
+
+
 
         File fileCss = new File("css" + File.separator + "premierStyle.css");
         scene.getStylesheets().add(fileCss.toURI().toString());
